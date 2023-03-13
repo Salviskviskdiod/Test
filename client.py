909d5be97f2158ee7404c5ae9e067a6caf706873
Salviskviskdiod <127413785+Salviskviskdiod@users.noreply.github.com>
2023-03-13 06:52:50 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,135 +1,95 @@
+import os,sys, time, pickle
 import socket
-import threading, time
-import pickle
-import random
-import codecs
+import threading
+from getpass import getpass
+import codecs, math
 
-HOST = "localhost"
-PORT = 9999
+def create_thread(target):
+    thread=threading.Thread(target=target)
+    thread.daemon=True
+    thread.start()
+
+HOST = f"{input('Num: ')}.tcp.eu.ngrok.io"
+PORT = int(input("PORT: "))
+
+connection_established=False
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+    sock.connect((HOST,PORT))
+    connection_established=True
+except :
+    pass
+
+running = True
+
+account = None
 
 class Account:
-    def __init__(self, username: str, password: str):
-        self.online = False
-        self.username = username
-        self.password = password
-        self.points = 0
-        self.ppc = 1
-        self.pps = 0
-        self.client = None
-class Item:
-    def __init__(self, name, ppc):
-        self.name = name
-        self.ppc = ppc
-banana = Item(1.3)
-class Box:
-    def __init__(self, )
+    def __init__(self):
+        self.points = None
+        self.ppc = None
+        self.pps = None
+        self.dict = {0 : "points", 1 : "ppc", 2: "pps"}
 
-#if input("Load Y / N > ").lower() == "y":
-   # with open("save.txt", "r") as f:
-   #     accounts = pickle.loads(codecs.decode(f.read().encode(), "base64"))
-    #    print(accounts.points)
-     #   for x in accounts:
-       #     print(x.__dict__)
-#else:
- #   xalvass = Account("Xalvass", "Gurka")
-  #  jennie = Account("jennie", "jennie")
-
-   # accounts = [xalvass, jennie]
-
-xalvass = Account("Xalvass", "Gurka")
-jennie = Account("jennie", "jennie")
-
-accounts = [xalvass, jennie]
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-
-server.listen()
-
-def sec1():
-    global accounts
+def receive_data():
+    global turn,connection_established, running, account
     while True:
-        time.sleep(1)
-      #  with open("save.txt", "w") as f:
-       #     new = []
-       #     for i, account in enumerate(accounts):
-       #         a = globals()[f"Obj{i}"]= Account(account.username, account.password)
-       #         for x in account.__dict__:
-          #          setattr(a, x, getattr(account, x))
-       #         a.client = None
-            #    new.append(a)
-            #f.write(codecs.encode(pickle.dumps(new), "base64").decode())
-        for account in accounts:
-            if account.online:
-                send_data='{}-{}-{}-{}'.format("Info", codecs.encode(pickle.dumps([account.points, account.ppc, account.pps]), "base64").decode(), None, None).encode()
-                account.client.send(send_data)
-            account.points += account.pps
-
-sec1_thread = threading.Thread(target=sec1)
-sec1_thread.start()
-
-
-
-def handle(client, address):
-    account = None
-    while True:
-        try:
-            data=client.recv(1024).decode("utf-8")
+        #try:
+            data=sock.recv(1024).decode("utf-8")
             data=data.split('-')
-            if data[0] == "Disconnect":
-                account.online = False
-                send_data='{}-{}-{}-{}'.format("Shutdown", None, None, None).encode()
-                client.send(send_data)
+            if data[0] == "Connected":
+                input("Connected to server")
+            if data[0] == "Login":
+                input("Logged in")
+            if data[0] == "Shutdown":
+                running = False
                 break
-            if not account == None:
-                if data[0] == 'Points':
-                    account.points += account.ppc
-                if data[0] == "GetInfo":
-                    send_data='{}-{}-{}-{}'.format("GetInfo", codecs.encode(pickle.dumps([account.points, account.ppc, account.pps]), "base64").decode(), None, None).encode()
-                    client.send(send_data)
-                if data[0] == "Leaderboard":
-                    leaderboard = sorted(accounts, key=lambda x: x.points, reverse=True)
-                    new = ""
-                    for x in leaderboard:
-                        new += f"{x.username}: Points: {x.points}    Ppc: {x.ppc}    PPs: {x.pps}\n"
-                    send_data='{}-{}-{}-{}'.format("Leaderboard", new, None, None).encode()
-                    client.send(send_data)
-                if data[0] == "Buy Ppc":
-                    if account.points >= 20 * int(data[1]):
-                        account.points -= 20 * int(data[1])
-                        account.ppc += int(data[1])
-                if data[0] == "Buy Pps":
-                    if account.points >= 50 * int(data[1]):
-                        account.points -= 50 * int(data[1])
-                        account.pps += int(data[1])
+            if data[0] == "Leaderboard":
+                input(data[1])
+            if data[0] == "Info":
+                a = pickle.loads(codecs.decode(data[1].encode(), "base64"))
+                account = Account()
+                for i, x in enumerate(a):
+                    setattr(account, account.dict[i], int(x))
+        #except:
+         #   print("ahh")
+
+create_thread(receive_data)
+
+login = False
+
+while running:
+    try:
+        time.sleep(0.2)
+        user_input = input("> ")
+        if user_input == "Help":
+            input("Login: Login to An Account\nGet: Get Your Points\nBuy: Buy more Ppc (Cost: 20p) or Pps (Cost: 50p)\n")
+        if not login:
+            if user_input == "Login":
+                send_data='{}-{}-{}-{}'.format("Login", input("Username: "), getpass(), None).encode()
+                sock.send(send_data)
+                login = True
+        else:
+            if user_input == "Buy":
+                user_input = input("1: Buy Ppc\n2: Buy Pps\n> ")
+                if user_input == "1":
+                    i = math.floor(account.points / 20)
+                    send_data='{}-{}-{}-{}'.format("Buy Ppc", input(f"Num (Max: {i}): "), None, None).encode()
+                    sock.send(send_data)
+                if user_input == "2":
+                    i = math.floor(account.points / 50)
+                    send_data='{}-{}-{}-{}'.format("Buy Pps", input(f"Num (Max: {i}): "), None, None).encode()
+                    sock.send(send_data)           
+                continue
+            elif user_input == "Get":
+                if not account == None:
+                    input(f"Points: {account.points}\nPpc: {account.ppc}\nPps: {account.pps}")
+            elif user_input == "Leaderboard":
+                send_data='{}-{}-{}-{}'.format("Leaderboard", None, None, None).encode()
+                sock.send(send_data)
             else:
-                if data[0] == "Login":
-                    for x in accounts:
-                        if x.username == data[1] and x.password == data[2]:
-                            account = x
-                            account.online = True
-                            account.client = client
-                            send_data='{}-{}-{}-{}'.format("Login", None, None, None).encode()
-                            client.send(send_data)
-                            print(f"Client {address} Logged into account {account.username}")
-                            break
-        except:
-            print(f"Client {address} {'Account: None' if account == None else f'Account: {account.username}'} disconnected")
-            if not account == None:
-                account.online = False
-            client.close()
-            break
+                send_data='{}-{}-{}-{}'.format("Points", None, None, None).encode()
+                sock.send(send_data)
 
-def receive():
-    while True:
-        client, address = server.accept()
-        print(f"Connected with {str(address)}!")
-        #broadcast(f"Client {address} connected to the server".encode('utf-8'))
-        send_data='{}-{}-{}-{}'.format("Connected", None, None, None).encode()
-        client.send(send_data)
-        thread = threading.Thread(target=handle, args=(client, address))
-        thread.start()
-
-print(f"Server running on {HOST}")
-
-receive()
+    except:
+        print("ahh")
